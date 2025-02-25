c17e155790cfbe54a04cb2d1749b5c72c808868b
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-25 09:06:27 +0800

    caimito: Ship viperfx
    Change-Id: If7ecbe35ae2c167a0098e5cd65bab9b9e50e272a

--- lineage_caiman.mk
+++ lineage_caiman.mk
@@ -29,6 +29,8 @@ AXION_CAMERA_REAR_INFO := 50,48
 AXION_CAMERA_FRONT_INFO := 42
 AXION_MAINTAINER := rmp
 
+TARGET_INCLUDE_VIPERFX := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="caiman-user 15 AP4A.250205.002 12821496 release-keys" \
     BuildFingerprint=google/caiman/caiman:15/AP4A.250205.002/12821496:user/release-keys \
--- lineage_tokay.mk
+++ lineage_tokay.mk
@@ -29,6 +29,8 @@ AXION_CAMERA_REAR_INFO := 50,48
 AXION_CAMERA_FRONT_INFO := 10.5
 AXION_MAINTAINER := rmp
 
+TARGET_INCLUDE_VIPERFX := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="tokay-user 15 AP4A.250205.002 12821496 release-keys" \
     BuildFingerprint=google/tokay/tokay:15/AP4A.250205.002/12821496:user/release-keys \
